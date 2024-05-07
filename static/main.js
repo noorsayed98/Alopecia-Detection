c8e2aad6f4d40d736dda63d85ee2e4920d634c00
bizzle22 <154422383+bizzle22@users.noreply.github.com>
2024-05-07 19:57:07 +0530

    Add files via upload

---
+++ static/main.js
@@ -0,0 +1,51 @@
+const nav = document.querySelector("nav");
+const mobileNav = document.querySelector("nav.mobile-nav");
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
+const mobileMenuContainer = document.querySelector(".mobile-menu-container");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 60) {
+    nav.classList.add("scrolled");
+    mobileNav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+    mobileNav.classList.remove("scrolled");
+  }
+});
+
+menuIcon.addEventListener("click", () => {
+  mobileMenuContainer.classList.add("active");
+});
+
+closeIcon.addEventListener("click", () => {
+  mobileMenuContainer.classList.remove("active");
+});
+
+const selectImage = document.querySelector('.select-image');
+const inputFile = document.querySelector('#file');
+const imgArea = document.querySelector('.img-area');
+
+selectImage.addEventListener('click', function () {
+	inputFile.click();
+})
+
+inputFile.addEventListener('change', function () {
+	const image = this.files[0]
+	if(image.size < 2000000) {
+		const reader = new FileReader();
+		reader.onload = ()=> {
+			const allImg = imgArea.querySelectorAll('img');
+			allImg.forEach(item=> item.remove());
+			const imgUrl = reader.result;
+			const img = document.createElement('img');
+			img.src = imgUrl;
+			imgArea.appendChild(img);
+			imgArea.classList.add('active');
+			imgArea.dataset.img = image.name;
+		}
+		reader.readAsDataURL(image);
+	} else {
+		alert("Image size more than 2MB");
+	}
+})
